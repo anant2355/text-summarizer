c00bb800c81687a21980a0969a49259731d58b01
anant2355 <achalumu@depaul.edu>
2024-07-31 11:08:24 -0500

    requirements added

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format = '[%(asctime)s]: %(message)s:')
 
-project_name = 'testSummaraizer'
+project_name = 'textSummaraizer'
 
 list_files = [
     ".github/workflows/.gitkeep",
